dc7f5b5a2f32862c881f824975e8f444595649a0
Laëtitia PHIMMASANE <laetitia.phimmasane@epsi.fr>
2022-10-11 21:59:01 +0200

    button redirect home

--- server.js
+++ server.js
@@ -64,7 +64,6 @@ app.get('/process_get', function (req, res) {
 
 app.get('/index.html', function (req, res) {
     res.sendFile( __dirname + "/src/index.html" );
-    //res.send('Page Questionnaire');
 })
 
 
@@ -72,5 +71,5 @@ var app = app.listen(8081, function () {
     var host = app.address().address
     var port = app.address().port
 
-    console.log("Example server listening at http://127.0.0.1:8081", host, port)
+    console.log("Server listening at http://127.0.0.1:8081", host, port)
 })
